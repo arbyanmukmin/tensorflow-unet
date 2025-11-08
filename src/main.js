77bf0c71d44a71c51e445adffb851026c79727f0
Raditya Bagas Arbyan Mukmin <62523229+arbyanmukmin@users.noreply.github.com>
2025-11-09 03:32:08 +0700

    Refactor model initialization in main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import * as tf from '@tensorflow/tfjs';
 import { buildUNet } from './unet.js';
 
-// [noisy = 3, albedo = 3, normal = 2, motion = 2, history = 3, history_mask = 1] total = 14
-const modelBeautyTemporal = buildUNet([null, null, 14], 32, 4);
-modelBeautyTemporal.summary();
-
-// [noisy = 3, albedo = 3, normal = 2] total = 8
-// const modelBeauty = buildUNet([null, null, 8], 32, 4);
-// modelBeauty.summary();
+const model = buildUNet([128, 128, 3], 32, 4);
+model.summary();
